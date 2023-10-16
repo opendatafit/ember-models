30da5e45cd3ec15f995a69953ba9c1a12df67c24
James Wilmot <jameswilmot2000@gmail.com>
2023-10-16 17:39:14 +1100

    view model: added algorithm field

--- addon/models/view.ts
+++ addon/models/view.ts
@@ -1,10 +1,11 @@
 import Model, { attr } from '@ember-data/model';
 import Base from '@opendatafit/ember-models/models/base';
 
-import { GenericView } from '@opendatafit/types';
+import { GenericView, AlgorithmView } from '@opendatafit/types';
 
 export default class ViewModel extends Base {
   @attr() declare spec: GenericView['spec'];
   @attr() declare resources: GenericView['resources'];
   @attr('string') declare specType: GenericView['specType'];
+  @attr('string') declare algorithm: AlgorithmView['algorithm'];
 }
