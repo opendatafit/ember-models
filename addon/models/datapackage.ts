d0d6f478e74fdadbbc549baabc27a02f0dead782
James Wilmot <jameswilmot2000@gmail.com>
2023-11-15 15:00:04 +1100

    datapackage model: added repository field

--- addon/models/datapackage.ts
+++ addon/models/datapackage.ts
@@ -11,7 +11,11 @@ import ResourceModel from '@opendatafit/ember-models/models/resource';
 import ViewModel from '@opendatafit/ember-models/models/view';
 import DisplayModel from '@opendatafit/ember-models/models/display';
 
+import { Repository } from '@opendatafit/types';
+
 export default class DatapackageModel extends Base {
+  @attr() declare repository: Repository;
+
   @hasMany('resource', 
       { async: false, inverse: null }) 
       declare resources: AsyncHasMany<ResourceModel>;
